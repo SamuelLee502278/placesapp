1be9c86a0bb91b56ae1c9d63be3c8ee2b0b410d1
Samuel Lee <63028329+SamuelLee502278@users.noreply.github.com>
2020-12-21 13:10:37 -0600

    added delete to individual trip page

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 array = ["california", "texas"]
 tripitems = [{"name": "Walmart", "address": "1619 Heddon Falls Dr."}]
+dataclass = data.DataClass()
 
 @app.route('/')
 def home():
@@ -17,19 +18,22 @@ def trip():
     #when user clicks on trip, routes to the trip instance page
     return render_template("trip.html")
 
-@app.route('/tripdetails',methods = ['POST','GET'])
-def tripdetails():
-    if request.method == 'POST':
-        result_string = request.form.get("place")
-        result_array = data.findplace(result_string)
-        return render_template("trip_details.html", search_result = result_array, tripitems = tripitems)
-    print(tripitems)
-    return render_template("trip_details.html", search_result = None, tripitems = tripitems)
-
 @app.route('/add', methods = ['POST', 'GET'])
 def addtrip():
     array.append(request.form.get("tripname"))
     return redirect(url_for('home'))
+
+@app.route('/tripdetails',methods = ['POST','GET'])
+def tripdetails():
+    if request.method == 'POST':
+        result_string = request.form.get("place")
+        result_array = dataclass.findplace(result_string)
+        dataclass.storesearch(result_array)
+        return render_template("trip_details.html", search_result = dataclass.getsearch(), tripitems = tripitems)
+    if dataclass.getsearch() != None:
+        return render_template("trip_details.html", search_result = dataclass.getsearch(), tripitems = tripitems)
+    else:
+        return render_template("trip_details.html", search_result = None, tripitems = tripitems)
 
 @app.route('/addtripitem', methods = ['POST', 'GET'])
 def addtripitem():
@@ -40,9 +44,17 @@ def addtripitem():
     }
     return jsonify(itemlist)
 
-
-# @app.route('/delete', methods = ['POST', 'GET'])
-# def delete():
+@app.route('/deletetripitem', methods = ['POST', 'GET'])
+def deletetripitem():
+    delete_item = json.loads(request.form['output'])
+    for i in range(len(tripitems)):
+        if tripitems[i]['address'] == delete_item['address']:
+            del tripitems[i]
+            break
+    itemlist = {
+        "items": tripitems
+    }
+    return jsonify(itemlist)
 
 if __name__=="__main__":
     app.run(debug=True)
--- data.py
+++ data.py
@@ -1,40 +1,48 @@
 import requests
 from config import secret
 
-def textsearch(usersearch):
-    textsearch_array = []
-    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+ usersearch + "&key=" + secret
-    r = requests.get(url)
-    if r.status_code != requests.codes.ok:
-        print(f"Error code {r.status_code} fetching data from Google Places TextSearch API")
-        return None
-    content = r.json()["results"]
-    for item in content:
-        textsearch_array.append(item['place_id'])
-        print(item['place_id'])
-    return textsearch_array 
+class DataClass:
 
-def findplace(usersearch):
-    results = []
-    usersearch = "+".join(usersearch.split(" "))
-    textsearch_array = textsearch(usersearch) 
-    for candidates in textsearch_array:
-        url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=" + candidates + "&fields=formatted_address,name,geometry,photo,formatted_phone_number&key=" + secret
+    def __init__(self):
+        self.searchstore = None
+
+    def textsearch(self, usersearch):
+        textsearch_array = []
+        url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+ usersearch + "&key=" + secret
         r = requests.get(url)
         if r.status_code != requests.codes.ok:
-            print(f"Error code {r.status_code} fetching data from Google Places Detail API")
+            print(f"Error code {r.status_code} fetching data from Google Places TextSearch API")
             return None
-        content = r.json()['result']
-        place = {
-            "name" : content['name'],
-            "address" : content['formatted_address'],
-            "lat" : content['geometry']['location']['lat'],
-            "lng" : content['geometry']['location']['lng'],
-            # "photo": content['photo']
-        }
-        results.append(place)
-    return results
+        content = r.json()["results"]
+        for item in content:
+            textsearch_array.append(item['place_id'])
+            print(item['place_id'])
+        return textsearch_array 
 
+    def findplace(self, usersearch):
+        results = []
+        usersearch = "+".join(usersearch.split(" "))
+        textsearch_array = self.textsearch(usersearch) 
+        for candidates in textsearch_array:
+            url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=" + candidates + "&fields=formatted_address,name,geometry,photo,formatted_phone_number&key=" + secret
+            r = requests.get(url)
+            if r.status_code != requests.codes.ok:
+                print(f"Error code {r.status_code} fetching data from Google Places Detail API")
+                return None
+            content = r.json()['result']
+            place = {
+                "name" : content['name'],
+                "address" : content['formatted_address'],
+                "lat" : content['geometry']['location']['lat'],
+                "lng" : content['geometry']['location']['lng'],
+                # "photo": content['photo']
+            }
+            results.append(place)
+        return results
 
-if __name__=="__main__":
-    findplace()
+    def storesearch(self, store):
+        self.searchstore = store
+
+    def getsearch(self):
+        return self.searchstore
+
